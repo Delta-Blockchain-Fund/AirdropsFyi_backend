72bef89447dde9f5d373c053e7fa4f5a35d47b79
0xSmit <smit@coinbriefly.com>
2022-11-23 17:29:28 +0530

    fix address case sensitive

--- src/routes/airdrops.ts
+++ src/routes/airdrops.ts
@@ -1,20 +1,20 @@
-import { Token, WalletToken } from "../database";
-import { Request, Response } from "express";
+import { Token, WalletToken, sequelize } from '../database';
+import { Request, Response } from 'express';
 
 async function airdropsRoute(req: Request, res: Response) {
   const { address } = req.params;
   const walletTokens = await WalletToken.findAll({
-    where: { walletAddress: address },
-    attributes: ["walletAddress", "amount"],
+    where: sequelize.where(sequelize.fn('lower', sequelize.col('walletAddress')), sequelize.fn('lower', address)),
+    attributes: ['walletAddress', 'amount'],
     include: [
       {
         model: Token,
-        attributes: ["name", "symbol", "logoUrl", "claimUrl"],
+        attributes: ['name', 'symbol', 'logoUrl', 'claimUrl'],
       },
     ],
   }).catch((err) => {
     console.log(err);
-    return res.status(500).json({ error: "Internal server error" });
+    return res.status(500).json({ error: 'Internal server error' });
   });
   return res.json(walletTokens);
 }
